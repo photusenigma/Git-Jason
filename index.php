ac92f44eb319d874d44307eb8533265e583f1b94
David Colwell <dcolwell@vmware.com>
2012-09-21 18:35:26 -0400

    Adding initial index.php page
    Jason…feel free to change this as much as you want….we can always go
    back to this version if we ever need to!

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/*
+	This is a sample php document
+	It contains comments like this, some valid php, and some other junk and nonesense.
+	Chang it however you like!
+*/
+$simpleString = "hello world";
+$simpleArray = array("heh", "low", "whirled");
+
+?>
+
+<html>
+<title>Sam Pull Page</title>
+
+<body>
+	<h1>this here is one of them sample test pages...thingies.</h1>
+
+	<p><b><? echo "Hell-oh-world!"; ?></b></p>
+	
+	<br/>
+
+	<p><b>
+<?
+while (list($val) = each($simpleArray) {
+    echo "$val ";
+}
+?>
+	</b></p>
+	
+
+</body>
+</html>
