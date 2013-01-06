d4f826cfe3ad271a75394fce307e2ab6dbeb3bb1
David Colwell <dcdigital72@me.com>
2013-01-05 19:01:41 -0500

    Showing Jason some things about git

--- index.php
+++ index.php
@@ -26,7 +26,17 @@ while (list($val) = each($simpleArray) {
 }
 ?>
 	</b></p>
-	
+	<ul>
+		<li>Item 1</li>
+		<li>Item 2</li>
+		<li>Item 3</li>
+		<li>Item 4</li>
+		<li>Item 5</li>
+		<li>Item 6</li>
+		<li>Item 7</li>
+		<li>Item 8</li>
+	</ul>
 
+	<div id="fred" class="box8 red">This is my div</div>
 </body>
 </html>
